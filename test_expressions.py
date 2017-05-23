e35c722c5212fa44d4af93610060cd6d9698520d
srpanall <srpanall@gmail.com>
2017-05-23 12:52:53 -0400

    Created a file for evaluating expressions.
    Now equation_parser doesn't need to be updated to test expressions that break it and a list of expressions that caused errors can be readily maintained.

---
+++ test_expressions.py
@@ -0,0 +1,28 @@
+from equation_parser import disp_ans
+
+# bad_exp = '-4,029 - 0.25'
+# disp_ans(bad_exp)
+
+# exp1 = '5+sin(4*6(3-5))'
+# disp_ans(exp1)
+
+# exp1a = '5+sin(-4)'
+# disp_ans(exp1a)
+
+# bad1 = '8+100*-21+63'
+# disp_ans(bad1)
+
+# bad2 = '8+-100*-21+63'
+# disp_ans(bad2)
+
+# exp2 = '4,029 / -4'
+# disp_ans(exp2)
+
+# exp2a = '4,029 * - 0.25'
+# disp_ans(exp2a)
+
+neg_base = '-3^0.5'
+disp_ans(neg_base)
+
+neg_base2 = '8-3^2'
+disp_ans(neg_base2)
