05857fcb8a0b1f2c1e5037b80de233b1e432125a
srpanall <srpanall@gmail.com>
2017-07-19 12:58:13 -0400

    Updated files after running pylint and refactored some functions using regex.
    These changes should increase readability and hopefully make further changes easier to implement.

--- arithmatic.py
+++ arithmatic.py
@@ -1,10 +1,20 @@
-
+"""This module evaluates a list of numbers and binary operations and returns
+a list containing a single numeric value.
+"""
 import re
 import operator
 import fractions as frac
 
 
-# ### Evaluation functions
+BIN_OP_DICT = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+}
+
+MUL_DIV_RE = re.compile(r'([\*\/])')
+ADD_SUB_RE = re.compile(r'([\-\+])')
+DEC_RE = re.compile(r'\.(\d+)')
 
 
 def decimal_place_counter(number):
@@ -15,7 +25,7 @@ def decimal_place_counter(number):
     if num_str.count('.') == 0:
         return 0
     else:
-        return len(num_str) - 1 - num_str.index('.')
+        return len(DEC_RE.search(num_str).group(1))
 
 
 def eval_bin_expr(num_1, num_2, bin_op):
@@ -50,22 +60,16 @@ def eval_bin_expr(num_1, num_2, bin_op):
         return int(new_num_3)
 
 
-BIN_OP_DICT = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-}
-
-
-def eval_a_op_b(terms, op):
-    '''performs the first calculation according to the order
-    of operations in the terms'''
+def eval_a_op_b(terms, op_type):
+    """Performs the first calculation in the expression based on the order
+    of operations and returns an updated list of terms.
+    """
     test_string = str(terms)
 
-    if op == 'md':
-        bin_op = mul_div_re.search(test_string).group(0)
+    if op_type == 'md':
+        bin_op = MUL_DIV_RE.search(test_string).group(0)
     else:
-        bin_op = add_sub_re.search(test_string).group(0)
+        bin_op = ADD_SUB_RE.search(test_string).group(0)
 
     index_1 = terms.index(bin_op)
     num_1, num_2 = terms[index_1 - 1], terms[index_1 + 1]
@@ -77,39 +81,42 @@ def eval_a_op_b(terms, op):
 
 
 def eval_expon(terms):
-    # print(terms)
-    expr_terms = [x for x in terms]
-    i = expr_terms.index('^')
-    if expr_terms[i + 1] == '-':
-        expr_terms[i + 1] = -1 * expr_terms[i + 2]
-        del expr_terms[i + 2]
+    """Evaluates the first exponential expression in the expression and returns
+    an updated list of terms.
+    """
+    pow_dex = terms.index('^')
+    if terms[pow_dex + 1] == '-':
+        terms[pow_dex + 1] = -1 * terms[pow_dex + 2]
+        del terms[pow_dex + 2]
 
-    expr_terms[i - 1] = expr_terms[i - 1] ** expr_terms[i + 1]
+    terms[pow_dex - 1] = terms[pow_dex - 1] ** terms[pow_dex + 1]
 
-    del expr_terms[i: i + 2]
+    del terms[pow_dex: pow_dex + 2]
 
-    return expr_terms
-
-
-mul_div_re = re.compile(r'([\*\/])')
-add_sub_re = re.compile(r'([\-\+])')
+    return terms
 
 
 def evaluate_terms(terms):
+    """Evaluates an expression given as a list of numbers and operations
+    following the order of operations and returns a numeric value.
+    """
     expr_terms = [x for x in terms]
 
     while expr_terms.count('^') != 0:
         expr_terms = eval_expon(expr_terms)
 
-    while mul_div_re.search(str(expr_terms)) is not None:
+    while MUL_DIV_RE.search(str(expr_terms)) is not None:
         expr_terms = eval_a_op_b(expr_terms, 'md')
 
-    while add_sub_re.search(str(expr_terms)) is not None:
+    while ADD_SUB_RE.search(str(expr_terms)) is not None:
         expr_terms = eval_a_op_b(expr_terms, 'pm')
 
     return expr_terms[0]
 
 
-if __name__ == '__main__':
-    print(eval_a_op_b([1, '*', 3], 'md')[0])
-    print(evaluate_terms([2, '^', 4]))
+# num = '3.1415'
+
+# if __name__ == '__main__':
+    # print(decimal_place_counter(num))
+    # print(eval_a_op_b([1, '*', 3], 'md')[0])
+    # print(evaluate_terms([2, '^', 4]))
