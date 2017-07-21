b67776b272526f7718b813ad15aa6c6c8cb935b7
srpanall <srpanall@gmail.com>
2017-07-21 17:48:23 -0400

    Collected the regexs from the other files in the branch.
    This places all of the regular expressions in one place to check for DRYness and allow for review based on increased comfort level with regexes in generl.

--- reg_test.py
+++ reg_test.py
@@ -1,4 +1,50 @@
 import re
+import collections
+
+# Parser functions
+
+Token = collections.namedtuple('Token', ['typ', 'value', 'start', 'stop'])
+
+token_specification = [
+    ('NEGNUM', r'^(\-\d+\.?\d*)'),  # Negative integer or decimal number
+    ('NUMBER', r'\d+\.?\d*'),       # Integer or decimal number
+    ('PI', r'PI'),                  # PI
+    ('FUNC', r'`+'),                # Functions
+    ('OP', r'[+\-*/\^]'),           # Arithmetic operators
+    ('PARENS', r'\(_+\)'),          # Parenthetical terms
+    ('OTHER', r'[A-Za-z]+'),        # Any other words
+    # ('LRP', r'[\(\)]'),             # Any parentheses
+    ('MISMATCH', r'.'),             # Any other character
+]
+
+tok_reg = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+
+token_re = re.compile(tok_reg)
+
+mult_re = re.compile(r'(\d)(\()|(\d)([a-zA-Z])')
+
+comma_re = re.compile(r'(\d)\,(\d\d\d)')
+
+# arithmetic.py
+
+MUL_DIV_RE = re.compile(r'([\*\/])')
+ADD_SUB_RE = re.compile(r'([\-\+])')
+DEC_RE = re.compile(r'\.(\d+)')
+
+# Formatting Functions
+
+PAREN_RE = re.compile(r'\([^\(\)]+\)')
+
+MULT_RE = re.compile(r'(\d)(\()|(\d)([a-zA-Z])')
+
+MULTI_I_RE = re.compile(r'([+-])?'                           # sign of term
+                        r'(\d+\.\d*|\d+)?'                   # coeff
+                        r'(?<![A-HK-Z])(i+|j+)(?![A-HK-Z])'  # multiple i or j
+                        r'(\d+\.\d*|\d+)?',                  # coeff
+                        flags=re.I)
+
+# Complex 
+
 
 MULTI_I_RE = re.compile(r'([+-])?'                           # sign of term
                         r'(\d+\.\d*|\d+)?'                   # coeff
@@ -9,7 +55,7 @@ MULTI_I_RE = re.compile(r'([+-])?'                           # sign of term
 COMP_REG = [r'(?P<sign>[+-])?',                            # sign of term
             r'(?P<fac1>\d+\.\d*|\d+)?',                    # coeff
             r'(?<![A-HK-Z])(?P<im>(i+|j+))(?![A-HK-Z])',   # multiple i/j
-            r'(?P<fac2>\d+\.\d*|\d+)?'                    # coeff
+            r'(?P<fac2>\d+\.\d*|\d+)?'                     # coeff
             ]
 
 COMP_RE = re.compile(''.join(COMP_REG), flags=re.I)
@@ -25,18 +71,20 @@ names = ['sign', 'fac1', 'im', 'fac2']
 #     print([[name, matchobj.group(name)] for name in names
 #            if matchobj is not None])
 
-comma_re = r'(?<![A-Z]\()(\d{1,3}(?:\,\d{3})+)(?!\))'
-
+# comma_re = r'(?<![A-Z]\()(\d{1,3}(?:\,\d{3})+)[^\)]'
+comma_re = r'(\d{1,3}\,\d{3}(?:\,\d{3})?)\D?'
 COMMA_REG = re.compile(comma_re, flags=re.I)
 
 
 # def remove_comma_format(matchobj):
 
 expr = '1,234,567'
+expr_2 = 'sin(' + expr + ')'
 expr_3 = 'sin(' + expr + '+' + expr + ')'
 
 
-expr_2 = '0' * expr.count(',') + expr.replace(',', '')
+expr_a = '0' * expr.count(',') + expr.replace(',', '')
 
 print(COMMA_REG.search(expr).group(1))
+print(COMMA_REG.findall(expr_2))
 print(COMMA_REG.findall(expr_3))
