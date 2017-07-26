21bf49e165032389738695e84ffbda8a39e14d86
srpanall <srpanall@gmail.com>
2017-07-26 17:55:14 -0400

    Trimmed unneeded functions and imports, added organizational comments, and updated eval_a_op_b.
    Updates to eval_a_op_b revolved around subtraction, negative numbers, and the regex I used to find the next operation. Did file house cleaning to improve code readability.

--- arithmatic.py
+++ arithmatic.py
@@ -3,9 +3,8 @@ a list containing a single numeric value.
 """
 import re
 import operator
-import fractions as frac
-import decimal as dec
 
+# Constants
 
 BIN_OP_DICT = {
     '+': operator.add,
@@ -14,54 +13,10 @@ BIN_OP_DICT = {
     '/': operator.truediv
 }
 
+# Regular expressions used in module
+
 MUL_DIV_RE = re.compile(r'([\*\/])')
 ADD_SUB_RE = re.compile(r'([\-\+])')
-DEC_RE = re.compile(r'\.(\d+)')
-
-
-def decimal_place_counter(number):
-    """Given a number, finds the number of decimal places and
-    returns an integer.
-    """
-    num_str = str(number)
-    if num_str.count('.') == 0:
-        return 0
-    else:
-        return len(DEC_RE.search(num_str).group(1))
-
-
-def eval_bin_expr(num_1, num_2, bin_op):
-    """Evaluates an expression containing 2 numbers and a binary operator
-    returns a numerical value with the appropriate precision.
-    """
-    if bin_op == '/':
-        if isinstance(num_1, int) and isinstance(num_2, int):
-            return frac.Fraction(num_1, num_2)
-        # dec_1 = dec.Decimal(num_1)
-        return num_1 / num_2
-
-    return BIN_OP_DICT[bin_op](num_1, num_2)
-    # num_3 = BIN_OP_DICT[bin_op](num_1, num_2)
-
-    # num_1_places = decimal_place_counter(num_1)
-    # num_2_places = decimal_place_counter(num_2)
-    # num_3_places = decimal_place_counter(num_3)
-
-    # if bin_op == '*':
-    #     r_places = num_1_places * num_2_places
-    # else:
-    #     r_places = max(num_1_places, num_2_places)
-
-    # if num_3_places <= r_places:
-    #     return num_3
-    # else:
-    #     str_num_3 = str(num_3)
-    #     new_num_3 = str_num_3[:len(str_num_3) - r_places]
-
-    # if new_num_3.count('.') == 1:
-    #     return float(new_num_3)
-    # else:
-    #     return int(new_num_3)
 
 
 def eval_a_op_b(terms, op_type):
@@ -76,9 +31,19 @@ def eval_a_op_b(terms, op_type):
     else:
         bin_op = ADD_SUB_RE.search(test_string).group(0)
 
-    index_1 = terms.index(bin_op)
+    if bin_op == '-':
+        try:
+            index_1 = terms.index(bin_op)
+        except ValueError:
+            # print(terms)
+            index_1 = terms.index('+')
+            bin_op = '+'
+
+    else:
+        index_1 = terms.index(bin_op)
+
     num_1, num_2 = terms[index_1 - 1], terms[index_1 + 1]
-    terms[index_1 - 1] = eval_bin_expr(num_1, num_2, bin_op)
+    terms[index_1 - 1] = BIN_OP_DICT[bin_op](num_1, num_2)
 
     del terms[index_1: index_1 + 2]
 
@@ -119,11 +84,3 @@ def evaluate_terms(terms):
         expr_terms = eval_a_op_b(expr_terms, 'pm')
 
     return expr_terms[0]
-
-
-# num = '3.1415'
-
-# if __name__ == '__main__':
-    # print(decimal_place_counter(num))
-    # print(eval_a_op_b([1, '*', 3], 'md')[0])
-    # print(evaluate_terms([2, '^', 4]))
